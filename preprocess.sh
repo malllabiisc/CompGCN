32a8ba486407f7d7aac0f0b9c85a9ced112b0c43
Max Buchholz <Max.Buchholz@gmx.de>
2023-01-11 15:55:53 +0100

    Add codex datasets to preprocess.sh

--- preprocess.sh
+++ preprocess.sh
@@ -3,5 +3,8 @@ mkdir log
 mkdir checkpoints
 mkdir data
 
+unzip data_compressed/codex-l.zip -d data
+unzip data_compressed/codex-m.zip -d data
+unzip data_compressed/codex-s.zip -d data
 unzip data_compressed/FB15k-237.zip -d data
 unzip data_compressed/WN18RR.zip -d data
