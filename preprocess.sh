9fdc15ad61b7a02a5935760b416967da83228310
st172874 <raphael-weber-bvs@gmx.de>
2023-02-09 17:00:45 +0100

    Added a small test dataset

--- preprocess.sh
+++ preprocess.sh
@@ -6,5 +6,6 @@ mkdir data
 unzip data_compressed/codex-l.zip -d data
 unzip data_compressed/codex-m.zip -d data
 unzip data_compressed/codex-s.zip -d data
+unzip data_compressed/codex-test.zip -d data
 unzip data_compressed/FB15k-237.zip -d data
 unzip data_compressed/WN18RR.zip -d data
